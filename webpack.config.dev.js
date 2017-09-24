8118ca0d7cc6fce3481554d1d908f6ba8438902f
Khaled Maged <5aledmaged@gmail.com>
2017-09-24 07:05:14 +0200

    updated webpack dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,20 +1,27 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
 
 export default {
 	devtool: 'inline-source-map',
-	entry: path.resolve(__dirname, 'src', 'js', 'app.js'),
+	entry: {
+		vendor: path.resolve(__dirname, 'src', 'js', 'vendor.js'),
+		main: path.resolve(__dirname, 'src', 'js', 'app.js')
+	},
 	target: 'web',
 	output: {
 		path: path.resolve(__dirname, 'src'),
-		publicPath: '/js/',
-		filename: 'bundle.js'
+		publicPath: '/',
+		filename: '[name].js'
 	},
 	plugins: [
 		// Create HTML file that includes reference to bundled JS.
 		new HtmlWebpackPlugin({
 			template: 'src/index.html',
 			inject: true
+		}),
+		new webpack.ProvidePlugin({
+			'$': 'jquery'
 		})
 	],
 	module: {
