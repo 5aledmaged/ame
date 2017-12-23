2bc6bb0531baa9281686702965433e3b88020333
5aledmaged <5aledmaged@gmail.com>
2017-12-23 04:40:41 +0200

    use compression and cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import path from 'path';
+import compression from 'compression';
+import cors from 'cors';
 
 const ame = express();
 const port = process.env.PORT ||5000;
 
-ame.use(express.static(path.resolve(__dirname, 'public')))
+ame.use(cors())
+	.use(compression())
+	.use(express.static(path.resolve(__dirname, 'public')))
 	.get('/', (req, res) => res.render('index.html'))
 	.listen(port, (err) => {
 		if (err) {
