54423afe52a9479d437d0a03865181d7bcaecc00
5aledmaged <5aledmaged@gmail.com>
2017-12-23 05:11:54 +0200

    configured post verb fot /cities

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import path from 'path';
 import compression from 'compression';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import fs from 'fs';
 
 const ame = express();
 const port = process.env.PORT ||5000;
@@ -12,7 +13,20 @@ ame.use(compression());
 ame.use(bodyParser.urlencoded({ extended: true }));
 ame.use(express.static(path.resolve(__dirname, 'public')));
 
-ame.get('/', (req, res) => res.render('index.html'));
+ame.get('/', (req, res) => { res.render('index.html') });
+
+ame.post('/cities', function (req, res) {
+	const id = req.body.id; // country
+	fs.readFile(path.join(__dirname, 'public', 'data', 'city.json'), 'utf-8', function (err, cities) {
+		if (err) {
+			res.status(500).send(err);
+		}
+		else {
+			cities = JSON.parse(cities)[id]; // get all cities of country
+			res.json(cities);
+		}
+	});
+});
 
 ame.listen(port, (err) => {
 	if (err) {
