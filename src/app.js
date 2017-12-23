88e8ca91322c3c0808a2b1c747549597219a4982
5aledmaged <5aledmaged@gmail.com>
2017-12-23 05:00:51 +0200

    use body-parser

--- src/app.js
+++ src/app.js
@@ -2,19 +2,23 @@ import express from 'express';
 import path from 'path';
 import compression from 'compression';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const ame = express();
 const port = process.env.PORT ||5000;
 
-ame.use(cors())
-	.use(compression())
-	.use(express.static(path.resolve(__dirname, 'public')))
-	.get('/', (req, res) => res.render('index.html'))
-	.listen(port, (err) => {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			console.log('ame-app.heroku.com');
-		}
-	});
+ame.use(cors());
+ame.use(compression());
+ame.use(bodyParser.urlencoded({ extended: true }));
+ame.use(express.static(path.resolve(__dirname, 'public')));
+
+ame.get('/', (req, res) => res.render('index.html'));
+
+ame.listen(port, (err) => {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log('ame-app.heroku.com');
+	}
+});
