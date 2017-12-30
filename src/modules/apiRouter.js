a578f7cc341b288159d9c837e914403ea57bea64
5aledmaged <5aledmaged@gmail.com>
2017-12-30 06:09:00 +0200

    added apiRoute

---
+++ src/modules/apiRouter.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import RequestHandler from '../modules/request-handler';
+
+const apiRouter = express.Router();
+
+apiRouter.use( bodyParser.urlencoded({extended:true}) );
+
+apiRouter.route('/')
+	.get((req, res) => {
+		res.send('good job');
+	})
+	.post((req, res) => {
+		if (typeof req.body.loc === 'undefined' || !req.body.loc) {
+			return res.status(500).send('error loc is not specified');
+		}
+
+		const forecastLoader = new RequestHandler();
+		const loc = JSON.parse(req.body.loc);
+
+		const weatherProcess = (error, response, body) => {
+			if (error) console.log('err ', error);
+			// console.log('res ', res.statusCode);
+			const forecast = JSON.parse(body);
+			res.json(forecast);
+		};
+
+		forecastLoader.get('main', loc, weatherProcess);
+
+		//res.send(`loc is ${req.body.loc}`);
+	});
+
+export default apiRouter;
