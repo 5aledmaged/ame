370892c7fb1698ca880926255003cb725107a225
5aledmaged <5aledmaged@gmail.com>
2018-01-09 04:31:20 +0200

    reworked preferences module

--- src/js/modules/preferences.js
+++ src/js/modules/preferences.js
@@ -1,7 +1,44 @@
-const preferences = () => {
+import $ from 'jquery';
+import errorHandler from './error-handler';
 
+class Preferences {
+	constructor() {
+		this.current = {
+			unit: 'c',
+			location: undefined
+		};
+		this.unitElement = $('.option.unit');
+		this.locationElement = $('.option.location');
+	}
 
-	return {};
-};
+	load() {
+		if (window.localStorage) {
+			try {
+				let prefs = localStorage.loadItem('options');
+				prefs = JSON.parse(prefs);
+				Object.assign(this.current, prefs);
+				console.log('successfully loaded preferences from localstorage');
+				return true;
+			}
+			catch (e) {
+				errorHandler(e);
+				return false;
+			}
+		}
+		else {
+			// errorHandler('localStorage not supported');
+			return false;
+		}
+	}
 
-export default preferences;
+	switchUnits() {
+		if (this.current.unit === 'c') {
+			this.current.unit = 'f';
+		}
+		else if (this.current.unit === 'f') {
+			this.current.unit === 'c';
+		}
+	}
+}
+
+export default Preferences;
